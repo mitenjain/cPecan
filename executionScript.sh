e025b24db0b736f96dbc979bd494d6de7063f6f6
Miten Jain <miten@soe.ucsc.edu>
2016-04-13 14:12:15 -0700

    shell wrapper for paths

---
+++ executionScript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+# Any subsequent(*) commands which fail will cause the shell script to exit immediately
+
+#Get directory containing shell script
+BASEDIR=$(dirname $0)/../
+
+#Set the python path to just the relevant directories
+export PYTHONPATH=${BASEDIR}
+
+#Set the path to just the relevant directories
+export PATH=${BASEDIR}/sonLib/bin:${BASEDIR}/jobTree/bin:${PATH}
+
+#Run described script
+python $@
+
